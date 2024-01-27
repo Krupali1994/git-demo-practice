065fda9ff230c433e7ade3724ca4c9828b51cc23
Krupali1994 <krupalicharlie2728@gmail.com>
2024-01-27 15:14:27 +0000

    commit3

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("My name is Krupali, How are you?");
+        System.out.println("My name is Krupali, Hello");
     }
 }
