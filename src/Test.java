99c5a732ea6235978a96d743596bcbaf37baf76a
Krupali1994 <147001464+Krupali1994@users.noreply.github.com>
2024-01-27 15:09:39 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My name is Krupali");
+        System.out.println("My name is Krupali, How are you?");
     }
 }
